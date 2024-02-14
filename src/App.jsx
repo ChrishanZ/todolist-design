ae3632f84b89002c30bf8e4583f7337cbd031557
chrishanz <chrisduttoo@gmail.com>
2024-02-14 01:36:23 +0100

    feature || first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Sample from "./components/Sample/Sample";
+
+function App() {
+  return (
+    <div className="d-flex justify-content-center align-items-center">
+      <Sample />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sample/Sample.jsx
@@ -0,0 +1,7 @@
+import styles from "./Sample.module.scss";
+
+function Sample() {
+  return <div className={styles.sample}>Sample</div>;
+}
+
+export default Sample;
