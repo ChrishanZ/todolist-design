f4ef4421b48504e1c770f8f9a3c41481b6284f4b
chrishanz <chrisduttoo@gmail.com>
2024-03-02 00:11:26 +0100

    dev || all seems good, added toaster everywhere

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,6 +1,10 @@
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import styles from "./Task.module.scss";
 import check from "../../assets/images/check.svg";
 import checked from "../../assets/images/checked.svg";
+import cross from "../../assets/images/cross.svg";
 
 function Task({ task, done, todolist, setTodolist, id }) {
   const toggleTask = (id) => {
@@ -14,15 +18,39 @@ function Task({ task, done, todolist, setTodolist, id }) {
     setTodolist(updatedList);
   };
 
+  const toggleDelete = (id) => {
+    const updatedList = todolist.filter((item) => item.id !== id);
+    setTodolist(updatedList);
+  };
+
+  const notifySuccess = () =>
+    toast.success("Tasks as been deleted.", {
+      position: "top-right",
+      theme: "light",
+    });
+
   return (
     <div
       className={`d-flex align-items-center ${
         done ? styles.taskdone : styles.task
       }`}
-      onClick={() => toggleTask(id)}
+      onClick={(e) => {
+        e.stopPropagation();
+        toggleTask(id);
+      }}
     >
       <img src={done ? checked : check} alt="check" />
       <p>{task}</p>
+      <img
+        src={cross}
+        onClick={(e) => {
+          e.stopPropagation();
+          toggleDelete(id);
+          notifySuccess();
+        }}
+        className={styles.cross}
+        alt="cross"
+      />
     </div>
   );
 }
